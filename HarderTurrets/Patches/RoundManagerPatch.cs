290f109d757c3cc8844233829691ba6d282c4a3e
twoandtwelve <jackykim573@gmail.com>
2024-03-12 03:04:20 +1300

    Add project files.

---
+++ HarderTurrets/Patches/RoundManagerPatch.cs
@@ -0,0 +1,53 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using SigurdLib.Util.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace HarderTurrets.Patches
+{
+    [HarmonyPatch(typeof(RoundManager))]
+    internal class RoundManagerPatch
+    {
+
+        [HarmonyPatch("SpawnMapObjects")]
+        [HarmonyTranspiler]
+        static IEnumerable<CodeInstruction> HazardSpawnPatch(IEnumerable<CodeInstruction> instructions)
+        {
+
+            var instructionList = instructions.ToList();
+
+            for (int i = 0; i < instructionList.Count; i++)
+            {
+                var instruction = instructionList[i];
+
+                if (instruction.opcode == OpCodes.Conv_I4)
+                {
+                    var constantLdcI4Instruction = new CodeInstruction(OpCodes.Ldc_I4, 3);
+                    var addInstruction = new CodeInstruction(OpCodes.Add);
+                    var multiplierLdcI4Instruction = new CodeInstruction(OpCodes.Ldc_I4, HarderTurrets.Instance.turretSpawnMultiplier.Value);
+                    var mulInstruction = new CodeInstruction(OpCodes.Mul);
+
+                    instructionList.Insert(i + 1, constantLdcI4Instruction);
+                    instructionList.Insert(i + 2, addInstruction);
+                    instructionList.Insert(i + 3, multiplierLdcI4Instruction);
+                    instructionList.Insert(i + 4, mulInstruction);
+
+                    break;
+                }
+            }
+
+
+            return instructionList;
+        }
+
+    }
+}
---
+++ HarderTurrets/Plugin.cs
@@ -0,0 +1,58 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarderTurrets.Patches;
+using HarmonyLib;
+using LethalConfig.ConfigItems.Options;
+using LethalConfig.ConfigItems;
+using LethalConfig;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarderTurrets
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    [BepInDependency("ainavt.lc.lethalconfig")]
+    public class HarderTurrets : BaseUnityPlugin
+    
+    
+    {
+        private const string modGUID = "Jacky.HarderTurrets";
+        private const string modName = "HarderTurrets";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        public static HarderTurrets Instance;
+
+        public ConfigEntry<int> turretSpawnMultiplier;
+
+        public static ManualLogSource logger;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            logger = base.Logger;
+
+            logger.LogInfo("Harder turrets has awaken");
+
+            turretSpawnMultiplier = Config.Bind("HarderTurrets Config", "Turret Spawn Multiplier", 2, "This value multiplies the number of Turret spawn!");
+            var turretSpawnMultiplierSlider = new IntSliderConfigItem(turretSpawnMultiplier, new IntSliderOptions
+            {
+                Min = 0,
+                Max = 30
+            });
+            LethalConfigManager.AddConfigItem(turretSpawnMultiplierSlider);
+
+
+            harmony.PatchAll(typeof(RoundManagerPatch));
+        }
+    }
+}
